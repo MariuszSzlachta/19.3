975e110bd7f5d99bd176b63fcd78a0ec97e1cbf4
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2018-11-02 16:50:07 +0100

    Add readme

--- TEST2.js
+++ TEST2.js
@@ -72,7 +72,7 @@ function reducer(state = initialState, action) {
       return Object.assign({}, state, {
         comments: state.comments.filter(comment => comment.id !== action.id)
       })
-    
+
     case EDIT_COMMENT:
       return Object.assign({}, state, {
         comments: state.comments.map(comment => {
